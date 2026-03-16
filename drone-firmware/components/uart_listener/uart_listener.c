9d3a7eba586d1b6937a67f5b236800ceb2ade57b
Simon Ross <simonross@cedarville.edu>
2026-03-16 15:05:31 -0400

    modularize functions, cleanup comments and spacing

--- drone-firmware/components/uart_listener/uart_listener.c
+++ drone-firmware/components/uart_listener/uart_listener.c
@@ -3,6 +3,7 @@
 #include "freertos/projdefs.h"
 #include "freertos/task.h"
 //#include "driver/uart.h"
+#include <stddef.h>
 #include <stdint.h>
 #include <sys/_intsup.h>
 #include <sys/_types.h>
@@ -14,8 +15,8 @@
 #include "uart_listener.h"
 #include "esp_log_level.h"
 
-static const unsigned int BUF_SIZE = 32; //TODO increase until you start getting errors?? FIXME FIXME
-static const unsigned int MAX_FILENAME_SIZE = 64;
+static const unsigned int BUF_SIZE = 32; //starts to have odd uart problems with larger numbers
+static const unsigned int MAX_FILENAME_SIZE = 64; 
 static const unsigned int MAX_UART_RETRIES = 1000;
 static const char *TAG = "uart_listener"; //I should probably use or lose this
 static TaskHandle_t listener_handle = NULL;
@@ -40,6 +41,15 @@ int read_bytes(uint8_t* buf, int nbyte) {
    return total;
 }
 
+int read_until(uint8_t* search_term, size_t len) {
+   uint8_t status_buf[len]; //FIXME not initialized to 0 (bad bad) and not a sliding window
+   while (memcmp(status_buf, search_term, len)!=0) { //TODO this is bad and assumes perfect alignment...read a byte at a time? running buffer??
+      read_bytes(status_buf,len);
+      vTaskDelay(pdMS_TO_TICKS(10)); //TODO: needed so we don't starve while waiting for input
+   }
+   return 0; //TODO no timeout whatsoever
+}
+
 void drain_rxbuf(void) {
    uint8_t buf[1024];
    while (read(0, buf, sizeof(buf)) > 0);
@@ -47,17 +57,13 @@ void drain_rxbuf(void) {
 
 void listener_task(void *pvParameter)
 {
-   uint8_t status_buf[6] = {0};
    uint8_t buf[BUF_SIZE];
    uint16_t filename_len;
    uint32_t file_size;
    drain_rxbuf();
-   
+   read_until((uint8_t*)"READY\n",6);
    write(1, "READY\n", 6);
-   while (memcmp(status_buf, "START\n", 6)!=0) { //TODO this is bad and assumes perfect alignment...read a byte at a time? running buffer??
-      read_bytes(status_buf,6); //FIXME FIXME failing here when nothing is sent because of infinite loop in read function above.
-      vTaskDelay(pdMS_TO_TICKS(10)); //TODO: needed? probably...
-   }
+   read_until((uint8_t*)"START\n",6);
    read_bytes((uint8_t*)&filename_len, 2);  //TODO: is this the right endianness??? probably reading in ascii
    if (filename_len > MAX_FILENAME_SIZE) return; //TODO use an error statement for the ESP
    char filename[MAX_FILENAME_SIZE+1]; //make sure the python checks your filename len
@@ -71,7 +77,7 @@ void listener_task(void *pvParameter)
    while (total_bytes < file_size) {
       int to_read = BUF_SIZE > (file_size-total_bytes) ? (file_size-total_bytes) : BUF_SIZE;
 
-      int len = read_bytes(buf,to_read); //THIS SHOULD USE THE READ_FUNCTION ABOVE?? uart_read_bytes is reading from console TODO make sure we read less than 32 bytes if less than 32 left
+      int len = read_bytes(buf,to_read);
       if (len <= 0) {
          esp_log_level_set("*", ESP_LOG_INFO); //TODO bad place for this
          ESP_LOGE(TAG, "Error receiving flightpath file");
@@ -83,7 +89,7 @@ void listener_task(void *pvParameter)
       total_bytes+=len;
    }
    close_flightpath();
-   while(1) { //FIXME FIXME need to delete this task somewhere, maybe here??? causes errors
+   while(1) { //FIXME FIXME need to delete this and return, but currently causes crashing...maybe delay then return??
       vTaskDelay(pdMS_TO_TICKS(10));
    }
 }
@@ -97,30 +103,26 @@ void uart_listener_start(void)
 
 void uart_listener_stop(void)
 {
-   //TODO if file transfer is still happening, error and fail.
+   //TODO if file transfer is still happening, error and abort.
    // Stop listener task first so it doesn't fight for UART
    if (listener_handle != NULL) {
       vTaskDelete(listener_handle);
       listener_handle = NULL;
    }
    
-   FILE *f = fopen("/littlefs/commands.txt", "rb"); //TODO remove some of this
+   FILE *f = fopen("/littlefs/commands.txt", "rb"); //TODO remove this chunk after making sure file naming works
    if (f != NULL) {
-
       uint8_t buf[1024] = {0};   // zero-filled buffer
-
       // Read up to 1024 bytes
       fread(buf, 1, sizeof(buf), f);
-
       fclose(f);
-
       // Always send exactly 1024 bytes
       write(1, buf, sizeof(buf));
-
    } else {
       const char *msg = "Failed to open file\n"; //TODO what is this garbage
       write(1, msg, strlen(msg));
    }
+
 
    esp_log_level_set("*", ESP_LOG_INFO); //TODO bad place for this
 }
