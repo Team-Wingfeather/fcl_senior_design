fcf8f72585e7689a0dcb035b60782c1002b5b5b0
Simon Ross <simonross@cedarville.edu>
2026-04-03 16:56:52 -0400

    add working onboard commander test

--- drone-firmware/components/control/control.c
+++ drone-firmware/components/control/control.c
@@ -1,14 +1,28 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include <string.h>
 
 #include "commander.h"
 #include "crtp_commander_high_level.h"
+#include "projdefs.h"
+#include "stabilizer_types.h"
 
 static TaskHandle_t script_control_handle = NULL;
-static const char *TAG = "script_control";
+static const int COMMAND_RATE_HZ = 50;  // Setpoint update rate
+
 
 void drone_script_control(void *pvParameter) {
-    //crtpCommanderHighLevelTakeoff(3,2);
+    //crtpCommanderHighLevelTakeoff(0.3,2);
+    setpoint_t sp;
+    state_t state;
+    memset(&sp, 0, sizeof(sp));
+    memset(&state, 0, sizeof(state));
+    commanderGetSetpoint(&sp, &state);
+    sp.thrust = 25000;
+    while(1) {
+        commanderSetSetpoint(&sp,COMMANDER_PRIORITY_CRTP);
+        vTaskDelay(pdMS_TO_TICKS(1000/COMMAND_RATE_HZ));
+    }
     vTaskDelete(NULL);
 }
 
